73e88bf134c7c9da5b344a220919f7119f8081b2
nataliakif <nataliakif@gmail.com>
2022-02-06 09:20:42 +0200

    gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const makeGalleryList = images =>{
+      return images.map(element => { 
+        const galleryItemEl = document.createElement('div');
+        galleryItemEl.classList.add('gallery__item');
+        const galleryLinkEl = document.createElement('a');
+        galleryLinkEl.classList.add('gallery__link');
+        // galleryLinkEl.target ='modal';
+        galleryLinkEl.href = element.original;
+        const galleryImgEl = document.createElement('img');
+        galleryImgEl.classList.add('gallery__image');
+        galleryImgEl.src = element.preview;
+        galleryImgEl.alt = element.description;
+        galleryImgEl.dataset.source = element.original;
+        galleryItemEl.appendChild(galleryLinkEl).appendChild(galleryImgEl);
+         
+        return galleryItemEl;
+    });
+    }
+    const gallery = document.querySelector('.gallery');
+    gallery.append(...makeGalleryList(galleryItems));
+
+gallery.addEventListener('click', openPhotoClick);
+
+function openPhotoClick(event) {
+
+   if( event.target.nodeName !== 'IMG'){
+    return;
+}
+event.preventDefault();
+
+const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="1140px" heigth ="744px">
+`)
+ instance.show();
+    if(basicLightbox.visible()){
+        window.addEventListener('keydown', closeModal);
+        function closeModal(event){
+            if(event.code === 'Escape'){
+                console.log(event.code)
+                instance.close()
+                window.removeEventListener('keydown', closeModal);
+            }
+    }   
+ }
+}
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const makeGalleryMarkup = (elements) => {
+  return elements.map(({ preview, original, description }) => {
+      return  `
+            
+            <a class="gallery__item" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+            />
+            </a> `;
+}).join('');
+};
+const gallery = document.querySelector('.gallery');
+gallery.insertAdjacentHTML('beforeend', makeGalleryMarkup(galleryItems));
+
+gallery.addEventListener('click', openPhotoClick);
+
+function openPhotoClick(event) {
+event.preventDefault();}
+
+let lightBox = new SimpleLightbox('.gallery a');
+lightBox.on('show.simplelightbox', function () {
+	console.log('traraar')
+});
+
+gallery.on('error.simplelightbox', function (e) {
+	console.log(e); // some usefull information
+});
+    
+        
